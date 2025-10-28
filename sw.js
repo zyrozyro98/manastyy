c88fdb61877d91a45f3525426b415f25da7d158d
zyrozyro98 <zyrozyro98@gmail.com>
2025-10-28 21:56:52 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,30 @@
+// sw.js - Service Worker للتطبيق
+const CACHE_NAME = 'educational-platform-v2';
+const urlsToCache = [
+  '/',
+  '/css/css.css',
+  '/js/config.js',
+  '/js/app.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;500;600;700&display=swap'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
